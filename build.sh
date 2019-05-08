a69c109a37487b1a6184ce716285c39d72fdcd98
Luke Yeager <lyeager@nvidia.com>
2019-05-08 10:12:32 -0700

    Try using libpmix-dev package on bionic
    Still not working. Configure says it can't be found

--- build.sh
+++ build.sh
@@ -2,38 +2,41 @@
 set -ex
 
 SLURM_VERSION=18.08.6-2
-UBUNTU_CODENAMES='xenial bionic'
-CENTOS_RELEASES='7'
 
 NAME=slurm-build
 DIST_DIR=./dist
 
-for codename in $UBUNTU_CODENAMES
-do
-        docker build --pull -t "$NAME" \
-                --file=./Dockerfile.ubuntu \
-                --build-arg SLURM_VERSION="$SLURM_VERSION" \
-                --build-arg UBUNTU_CODENAME="$codename" \
-                .
+build () {
+        docker build --pull -t "$NAME" --file="$1" --build-arg SLURM_VERSION="$SLURM_VERSION" .
         docker ps -q -a -f "name=$NAME" | xargs -r docker rm
         docker create --name="$NAME" "$NAME"
         rm -rf "$DIST_DIR"
         docker cp "${NAME}:/dist" "$DIST_DIR"
         docker rm "$NAME"
         cp "$DIST_DIR"/* .
-done
+}
 
-for release in $CENTOS_RELEASES
-do
-        docker build --pull -t "$NAME" \
-                --file=./Dockerfile.centos \
-                --build-arg SLURM_VERSION="$SLURM_VERSION" \
-                --build-arg CENTOS_RELEASE="$release" \
-                .
-        docker ps -q -a -f "name=$NAME" | xargs -r docker rm
-        docker create --name="$NAME" "$NAME"
-        rm -rf "$DIST_DIR"
-        docker cp "${NAME}:/dist" "$DIST_DIR"
-        docker rm "$NAME"
-        cp "$DIST_DIR"/* .
-done
+build_bionic () {
+        build Dockerfile.bionic
+}
+
+build_centos7 () {
+        build Dockerfile.centos7
+}
+
+case "$1" in
+        bionic)
+                build_bionic
+                ;;
+        centos7)
+                build_centos7
+                ;;
+        all)
+                build_bionic
+                build_centos
+                ;;
+        *)
+                echo "Usage: $0 [bionic|centos7|all]"
+                exit 1
+                ;;
+esac
